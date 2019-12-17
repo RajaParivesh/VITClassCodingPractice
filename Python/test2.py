ee5b1da6bcb8773e70c18537a78f0b1d39155d23
RajaParivesh <rajaparivesh@gmail.com>
2019-12-18 00:29:13 +0530

    Convert md file to jupyter notebook

---
+++ Python/test2.py
@@ -0,0 +1,13 @@
+# perfect number
+
+n = int(input())
+sum = 0
+for i in range(1, n//2+1):
+    if n % i == 0:
+        sum += i
+if sum == n:
+    print("perfect")
+else:
+    print("Not perfect")
+
+# Amicable
